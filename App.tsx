4ea160d45818d1d3469e51a8f9372c7e8bbbc39c
Raju Dhage <rajudhage@Rajus-MacBook-Air.local>
2025-12-29 16:24:16 +0530

    RN init

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { Text, TextInput, Button} from 'react-native';
+
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'light';
 
   return (
     <SafeAreaProvider>
@@ -24,22 +17,48 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+ 
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.text}>Welcome</Text>
+      <TextInput style={styles.input} placeholder="Type here..." />
+      <Button title="Press me" onPress={() => {}} />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#f0f0f0',
+    borderWidth: 2,
+    borderColor: '#66ffa6ff',
   },
+  text:{
+    fontSize: 24,
+  },
+  input:{
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '40%',
+    marginTop: 20,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  button:{
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: 'blue',
+    color: 'white',
+    textAlign: 'center',
+    borderRadius: 5,
+  }
 });
 
 export default App;
